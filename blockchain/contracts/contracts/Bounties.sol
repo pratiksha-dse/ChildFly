5643ed81419ace4879739252fb19178be6fecb54
Shashwat Sharma <shashwats72@gmail.com>
2022-05-22 01:02:42 +0530

    solidity added

--- blockchain/contracts/contracts/Bounties.sol
+++ blockchain/contracts/contracts/Bounties.sol
@@ -2,16 +2,26 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Bounties{
-    string public name = "Virtual Ether";
-    event Transfer(
-        address indexed _sender,
-        address indexed _receiver,
-        uint amt
-    );
-    event Approve(
-        address indexed _owner,
-        address indexed _spender,
-        uint amt
-    );
-
+    address public owner;
+    address public recipient;
+    uint public admin_amt;
+    mapping (address => uint) prizes;
+    constructor() payable public {
+        owner = msg.sender;
+        admin_amt = msg.value;
+    }
+    modifier isOwner{
+        require(msg.sender == owner);
+        _;
+    }
+    event Sent(address _from, address _to, uint amt);
+    function givePrize(address payable _receiver, uint amt) public payable{
+        recipient = _receiver;
+        _receiver.transfer(amt);
+        prizes[_receiver] += amt;
+        emit Sent(msg.sender, recipient, amt);
+    }
+    function print_prize() public view returns(uint) {
+        return prizes[recipient];
+    }   
 }
